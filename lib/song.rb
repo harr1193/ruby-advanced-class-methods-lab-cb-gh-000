b9ff8db01601bcda8c76182fe94cca91f244d205
Kyle Harris <k_harris95@live.com>
2018-01-31 01:35:01 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,6 +34,6 @@ class Song
   end
 
   def self.find_or_create_by_name(name)
-    song = self.create_by_name(name) ||song = self.find_by_name(name)
+    self.create_by_name(name) || self.find_by_name(name)
   end
 end
