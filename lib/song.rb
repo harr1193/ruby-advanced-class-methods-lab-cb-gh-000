2be13eb78702ebff09a6b39267c7f82dd7b22ed5
Kyle Harris <k_harris95@live.com>
2018-01-31 01:51:30 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -49,4 +49,20 @@ class Song
     song.artist_name = artist_name
     song
   end
+
+  def self.create_from_filename(filename)
+    seperator = filename.split(" - ")
+    artist_name = seperator[0]
+    song_name = seperator[1].gsub(".mp3", "")
+
+    song = self.create
+    song.name = song_name
+    song.artist_name = artist_name
+    song
+
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
 end
